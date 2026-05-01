09797b2fe53cf29edaf098b54e0a3a22027e41d2
M. Barbosa <maatheusb96@gmail.com>
2026-05-01 13:43:50 -0300

    add CONFLICT_FOUND

--- src/main/scala/br/unb/cic/soot/graph/Graph.scala
+++ src/main/scala/br/unb/cic/soot/graph/Graph.scala
@@ -345,8 +345,10 @@ class Graph() {
 
       val foundedPaths = findPathsOP(sourceNode, sourceNode, sinkNode, HashSet(sourceNode), ignoredNodes, maxConflictsNumber)
       val validPaths = foundedPaths.filter(path => isValidPath(sourceNode, sinkNode, path))
-      if (validPaths.nonEmpty)
+      if (validPaths.nonEmpty) {
+        println("[CONFLICT_FOUND]")
         paths = paths ++ List(validPaths.head)
+      }
     }
 
     return paths
@@ -608,6 +610,7 @@ class Graph() {
           val paths = findPath(source, sink)
           val pathsHaveSameSourceAndSinkRootTraversedLine: Boolean = conflicts.exists(c => paths.exists(p => c.head.line() == p.head.line() && c.last.line() == p.last.line()))
           if (!pathsHaveSameSourceAndSinkRootTraversedLine){
+            paths.foreach(_ => println("[CONFLICT_FOUND]"))
             conflicts = conflicts ++ paths
           }
         })
