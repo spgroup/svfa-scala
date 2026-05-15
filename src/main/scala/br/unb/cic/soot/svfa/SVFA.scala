d16a433b701fe91763b04d52bba3f80c1fe427c6
M. Barbosa <maatheusb96@gmail.com>
2026-05-15 09:03:01 -0300

    feat: add abstract SVFA base class to handle Soot configuration and graph construction

--- src/main/scala/br/unb/cic/soot/svfa/SVFA.scala
+++ src/main/scala/br/unb/cic/soot/svfa/SVFA.scala
@@ -1,7 +1,11 @@
 package br.unb.cic.soot.svfa
 
 
+import com.google.common.base.Stopwatch
 import soot._
+
+import java.util.Collections
+import java.util.concurrent.TimeUnit
 
 /**
  * Base class for all implementations
@@ -11,14 +15,24 @@ abstract class SVFA extends SootConfiguration {
 
   var svg = new br.unb.cic.soot.graph.Graph()
 
+  var packageExecutionTimes = new java.util.HashMap[String, java.lang.Long]()
+
   def buildSparseValueFlowGraph() {
     configureSoot()
     beforeGraphConstruction()
     val (pack, t) = createSceneTransform()
     PackManager.v().getPack(pack).add(t)
-    configurePackages().foreach(p => PackManager.v().getPack(p).apply())
+    configurePackages().foreach(p => {
+      val stopwatch = Stopwatch.createStarted
+      PackManager.v().getPack(p).apply()
+      val elapsedMs = stopwatch.elapsed(TimeUnit.MILLISECONDS)
+      packageExecutionTimes.put(p, elapsedMs)
+    })
     afterGraphConstruction()
   }
+
+  def getPackageExecutionTimes: java.util.Map[String, java.lang.Long] =
+    Collections.unmodifiableMap(packageExecutionTimes)
 
   def svgToDotModel(): String = {
     svg.toDotModel()
